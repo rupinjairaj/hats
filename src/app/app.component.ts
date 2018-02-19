5bfd5f50d3312ceb82c3c6072df489850c771091
Rupin Jairaj <rupin_jairaj@dell.com>
2018-02-19 09:19:33 +0530

    Add seprate lists for producers and consumers.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
+import { ListContainers, StartContainer, StopContainer } from './models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  producerList = new Array<ListContainers>()
+  consumerList = new Array<ListContainers>()
+
+  constructor(private _http: HttpService) {
+
+  }
+
+  ngOnInit() {
+    this.getContainerList()
+  }
+
+  getContainerList() {
+    this.consumerList = []
+    this.producerList = []
+    this._http.getContainerList().subscribe(
+      data => {
+        data.forEach(x => {
+          var producerContainer = x.image.includes("producer")
+          var consumerContainer = x.image.includes("consumer")
+          if (producerContainer) {
+            this.producerList.push(x)
+          } else if (consumerContainer) {
+            this.consumerList.push(x)
+          }
+          if (x.state == "running") {
+            x.toggle = true
+          } else {
+            x.toggle = false
+          }
+        })
+      },
+      err => console.log(err)
+    )
+  }
+
+  onContainerStateChange(x: ListContainers) {
+    if (x.toggle) {
+      console.log("in here")
+      var startMsg = new StartContainer()
+      startMsg.id = x.id.substring(0, 12)
+      startMsg.checkpointId = ""
+      startMsg.checkpointDir = ""
+      this._http.startContainerByID(startMsg).subscribe(
+        data => {
+          this.getContainerList()
+        },
+        err => console.log(err)
+      )
+    } else {
+      var stopMsg = new StopContainer()
+      stopMsg.id = x.id.substring(0, 12)
+      stopMsg.duration = 0
+      this._http.stopContainerByID(stopMsg).subscribe(
+        data => {
+          this.getContainerList()
+        },
+        err => console.log(err)
+      )
+    }
+  }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { HttpService } from '../http.service';
 import { ListContainers, StartContainer, StopContainer } from '../models';
 
@@ -11,51 +11,20 @@ export class ListComponent implements OnInit {
 
   constructor(private _http: HttpService) { }
 
-  list: ListContainers[]
+  @Input() list: ListContainers[]
+  @Output() onContainerStateChangeEvent:EventEmitter<ListContainers> = new EventEmitter<ListContainers>()
 
   ngOnInit() {
-    this.getContainerList()
-  }
-
-  getContainerList() {
-    this._http.getContainerList().subscribe(
-      data => {
-        this.list = data;
-        this.list.forEach(x => {
-          if (x.state == "running") {
-            x.toggle = true
-          } else {
-            x.toggle = false
-          }
-        })
-      },
-      err => console.log(err)
-    )
+    // this.getContainerList()
   }
 
   onContainerStateChange(x: ListContainers) {
-    x.toggle = !x.toggle
-    if (x.toggle) {
-      var startMsg = new StartContainer()
-      startMsg.id = x.id.substring(0, 12)
-      startMsg.checkpointId = ""
-      startMsg.checkpointDir = ""
-      this._http.startContainerByID(startMsg).subscribe(
-        data => {
-          this.getContainerList()
-        },
-        err => console.log(err)
-      )
-    } else {
-      var stopMsg = new StopContainer()
-      stopMsg.id = x.id.substring(0, 12)
-      stopMsg.duration = 0
-      this._http.stopContainerByID(stopMsg).subscribe(
-        data => {
-          this.getContainerList()
-        },
-        err => console.log(err)
-      )
-    }
+    this.list.forEach(element => {
+      if(element.id == x.id) {
+        element.toggle = !element.toggle
+      }
+    });
+    this.onContainerStateChangeEvent.emit(x)
   }
+
 }
